ea5ac154f83cee9e77b7dec3c447ed214eadac3c
两仪天心 <rampage@tennsinn.com>
2021-08-15 23:52:29 +0800

    create the Renamer class and set the independent entry of the program

---
+++ PyRenamer.py
@@ -0,0 +1,5 @@
+from Renamer import Renamer as ren
+
+if __name__ == '__main__':
+	renamer = ren()
+	renamer.rename_ref()
--- Renamer.py
+++ Renamer.py
@@ -1,18 +1,19 @@
 import os
 
-def rename_ref():
-	path_ren = input("Path Rename: ")
-	flist_ren = os.listdir(path_ren)
-	flist_ren.sort()
-	len_ren = len(flist_ren)
-	path_ref = input("Path Ref: ")
-	flist_ref = os.listdir(path_ref)
-	flist_ref.sort()
-	len_ref = len(flist_ref)
-	for i in range(min(len_ren,len_ref)):
-		ext = os.path.splitext(flist_ren[i])[1]
-		base = os.path.splitext(flist_ref[i])[0]
-		os.rename(path_ren+'/'+flist_ren[i], path_ren+'/'+base+ext)
+class Renamer():
+	def __init__(self):
+		pass
 
-if __name__ == '__main__':
-	rename_ref()
+	def rename_ref(self):
+		path_ren = input("Path Rename: ")
+		flist_ren = os.listdir(path_ren)
+		flist_ren.sort()
+		len_ren = len(flist_ren)
+		path_ref = input("Path Ref: ")
+		flist_ref = os.listdir(path_ref)
+		flist_ref.sort()
+		len_ref = len(flist_ref)
+		for i in range(min(len_ren,len_ref)):
+			ext = os.path.splitext(flist_ren[i])[1]
+			base = os.path.splitext(flist_ref[i])[0]
+			os.rename(path_ren+'/'+flist_ren[i], path_ren+'/'+base+ext)
