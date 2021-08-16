d504a209db9c0741adc3f30ca1be1f69f606d5d8
两仪天心 <rampage@tennsinn.com>
2021-08-16 23:06:35 +0800

    separate the process of getting the list of files into a function

--- Renamer.py
+++ Renamer.py
@@ -4,14 +4,17 @@ class Renamer():
 	def __init__(self):
 		pass
 
+	def get_files_in_path(self, path):
+		flist = os.listdir(path)
+		flist.sort()
+		return flist
+
 	def rename_ref(self):
 		path_ren = input("Path Rename: ")
-		flist_ren = os.listdir(path_ren)
-		flist_ren.sort()
+		flist_ren = self.get_files_in_path(path_ren)
 		len_ren = len(flist_ren)
 		path_ref = input("Path Ref: ")
-		flist_ref = os.listdir(path_ref)
-		flist_ref.sort()
+		flist_ref = self.get_files_in_path(path_ref)
 		len_ref = len(flist_ref)
 		for i in range(min(len_ren,len_ref)):
 			ext = os.path.splitext(flist_ren[i])[1]
