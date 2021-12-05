edf7a9932d6f5da78377147cd7623bdeefacab7c
两仪天心 <rampage@tennsinn.com>
2021-12-05 22:04:11 +0800

    fully refactor Renamer class

--- Renamer.py
+++ Renamer.py
@@ -1,66 +1,162 @@
 import os
 import time
 import re
+from typing import *
 
 class Renamer():
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.renp = ''
+        self.rens = []
+        self.refs = []
+        self.nrefs = []
+        self.ext = False
+        self.prefix = ''
+        self.suffix = ''
+        self.spr = ' '
+        self.start = False
 
-	def write_log(self, mode:str, name:str, args:dict, renlog:list):
-		if not os.path.exists('logs'):
-			os.mkdir('logs')
-		if not name:
-			name = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
-		with open('logs/'+name+'.log', 'w', encoding='utf-8') as f:
-			f.write('Mode='+mode+'\n')
-			for key, val in args.items():
-				f.write(key+'='+val+'\n')
-			for (old, new) in renlog:
-				f.write(old+'\t'+new+'\n')
+    @staticmethod
+    def validate_dir(path:str):
+        if not os.path.exists(path):
+            raise Exception("Dir does not exist!")
+        if not os.path.isdir(path):
+            raise Exception("Path is not a directory!")
 
-	def validate_path(self, path:str, allow:list):
-		if not os.path.exists(path):
-			raise Exception("Path does not exist!")
-		elif 'dir' not in allow and os.path.isdir(path):
-			raise Exception("Path can't be a directory!")
-		elif 'file' not in allow and os.path.isfile(path):
-			raise Exception("Path can't be a file!")
+    @staticmethod
+    def validate_file(path:str):
+        if not os.path.exists(path):
+            raise Exception("File does not exist!")
+        if not os.path.isfile(path):
+            raise Exception("Path is not a file!")
 
-	def get_names(self, path:str, ext:bool=True):
-		names = []
-		if os.path.isdir(path):
-			files = os.listdir(path)
-			if ext:
-				names = files
-			else:
-				for file in files:
-					names.append(os.path.splitext(file)[0])
-			names.sort()
-		elif os.path.isfile(path):
-			with open(path, 'r', encoding="utf-8") as f:
-				for name in f.readlines():
-					names.append(name.strip())
-		return names
+    @staticmethod
+    def filter_name(name:str):
+        name = re.sub(r'[\\/:*?"<>|]+', "_", name)
+        return name
 
-	def rename_ref(self, path_ren:str, path_ref:str):
-		flist_ren = self.get_names(path_ren)
-		len_ren = len(flist_ren)
-		flist_ref = self.get_names(path_ref, False)
-		len_ref = len(flist_ref)
-		renlog = []
-		for i in range(min(len_ren,len_ref)):
-			ext = os.path.splitext(flist_ren[i])[1]
-			os.rename(path_ren+'/'+flist_ren[i], path_ren+'/'+flist_ref[i]+ext)
-			renlog.append((flist_ren[i], flist_ref[i]+ext))
-		return renlog
+    def get_names_from_dir(self, path:str):
+        self.validate_dir(path)
+        dirs = os.listdir(path)
+        names = [self.filter_name(file) for file in dirs if os.path.isfile(os.path.join(path, file))]
+        names.sort()
+        return names
 
-	def rename(self, mode:str, args:dict={}, name:str=None):
-		name = re.sub(r'[\\/:*?"<>|\r\n]+', "_", name) if name else None
-		if 'RenameRef' == mode:
-			arg_list = [('PathRen', ['dir'], 'Path Rename: '), ('PathRef', ['dir','file'], 'Path Reference: ')]
-			for (key, allow, memo) in arg_list:
-				if key not in args:
-					args[key] = input(memo).replace('\\','/')
-					self.validate_path(args[key], allow)
-			renlog = self.rename_ref(args['PathRen'], args['PathRef'])
-			self.write_log('RenameRef', name, {'PathRen':args['PathRen'], 'PathRef':args['PathRef']}, renlog)
+    def get_names_from_file(self, path:str):
+        self.validate_file(path)
+        names = []
+        with open(path, 'r', encoding="utf-8") as f:
+            for name in f.readlines():
+                self.filter_name(name)
+                names.append(name.strip())
+        return names
+
+    def get_names_from_text(self, text:str):
+        text = self.filter_name(text)
+        text = text.strip('\n').strip('\r')
+        names = re.split('[\r\n]', text)
+        return names
+
+    def set_renp(self, renp:str):
+        self.renp = renp
+
+    def set_rens(self, rens:list):
+        self.rens = rens
+
+    def set_refs(self, refs:list):
+        self.nrefs = self.refs = refs
+
+    def set_ext(self, ext:Union[bool,str]):
+        if isinstance(ext, str):
+            ext = self.filter_name(ext)
+            ext = ext.strip('.')
+        self.ext = ext
+
+    def set_prefix(self, prefix:str=None):
+        self.prefix = self.filter_name(prefix)
+
+    def set_suffix(self, suffix:str=None):
+        self.suffix = self.filter_name(suffix)
+
+    def set_spr(self, spr:str=' '):
+        self.spr = self.filter_name(spr)
+
+    def set_start(self, num:Union[bool,int,str]=0, fill:Union[bool,int,str]=False):
+        self.start = str(num)
+        if fill:
+            self.start = self.start.zfill(int(fill))
+
+    def get_order(self, i:Union[int,str]):
+        return str(int(self.start)+int(i)).zfill(len(self.start))
+
+    def add_ext(self):
+        if not self.ext:
+            self.nrefs = [ref+os.path.splitext(ren)[-1] for ren, ref in zip(self.rens, self.nrefs)]
+        elif isinstance(self.ext, str):
+            self.nrefs = [ref+'.'+self.ext for ref in self.nrefs]
+
+    def add_order(self):
+        if self.start:
+            self.nrefs = [self.get_order(i)+self.spr+self.nrefs[i] for i in range(0, len(self.nrefs))]
+
+    def add_prefix(self):
+        if self.prefix:
+            self.nrefs = [self.prefix+self.spr+ref for ref in self.nrefs]
+
+    def add_suffix(self):
+        if self.suffix:
+            for i in range(0, len(self.nrefs)):
+                base, ext = os.path.splitext(self.nrefs[i])
+                self.nrefs[i] = base+self.spr+self.suffix+ext
+
+    def rename(self):
+        self.add_ext()
+        self.add_order()
+        self.add_prefix()
+        self.add_suffix()
+        os.chdir(self.renp)
+        for ren, nref in zip(self.rens, self.nrefs):
+            os.rename(ren, nref)
+
+    def log(self):
+        pass
+
+    def rollback(self, log):
+        pass
+
+
+if __name__ == '__main__':
+    r = Renamer()
+    renm = input('Rename Mode [dir / file / text]: ')
+    renp = input('Rename Path: ').replace('\\', '/')
+    r.set_renp(renp)
+    if 'dir' == renm:
+        rens = r.get_names_from_dir(renp)
+    elif 'file' == renm:
+        rens = r.get_names_from_file(input('Rename File: '))
+    else:
+        rens = r.get_names_from_text(input('Rename Text: '))
+    r.set_rens(rens)
+    refm = input('Reference Mode [dir / file / text]: ')
+    if 'dir' == refm:
+        refs = r.get_names_from_dir(input('Reference Path: ').replace('\\', '/'))
+    elif 'file' == refm:
+        refs = r.get_names_from_file(input('Reference Path: ').replace('\\', '/'))
+    else:
+        refs = r.get_names_from_text(input())
+    r.set_refs(refs)
+    r.set_prefix(input('Prefix: '))
+    r.set_suffix(input('Suffix: '))
+    flag = input('Replace ext? [y/n]: ')
+    if 'y' == flag:
+        ext = input('New ext: ')
+        if '' == ext:
+            r.set_ext(True)
+        else:
+            r.set_ext(ext)
+    flag = input('Auto Increment? [y/n]: ')
+    if 'y' == flag:
+        num = input('Start Number: ')
+        fill = input('Zero Fill: ')
+        r.set_start(num, fill)
+    r.set_spr(input('Separator: '))
+    r.rename()
